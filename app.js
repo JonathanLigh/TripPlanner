28f349e8d2fa4196c5f7d57dbb956e7656291cdd
Ray Furtado <rayfurtado11@gmail.com>
2016-06-27 17:20:09 -0400

    changes

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 var Express = require('express');
-var app = new Express();
-
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var app = new Express();
+
+
+
+
+app.use('/', function(req, res , next){
+	res.send('bye')
+	next();
+})
+
+
+
+app.use(function(req, res, next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err)
+})
+
+app.use(function(err, req, res ,next){
+	res.status(err.status || 500);
+	console.error(err);
+	res.send(
+		'ERROR')
+})
+
+app.listen(3000, function(){
+	console.log('app is now listening')
+})
