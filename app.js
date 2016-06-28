237a677e42594fd141b7d62d8af0085bb9655986
Ray Furtado <rayfurtado11@gmail.com>
2016-06-28 09:58:03 -0400

    swig

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app = new Express();
 var routes = require('./routes');
+var swig = require('swig');
 
 
+
+app.use(morgan('dev'));
 app.use('/', routes);
 
+
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.engine('html', swig.renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next){
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err)
 })
+
+
+
 
 app.use(function(err, req, res ,next){
 	res.status(err.status || 500);
